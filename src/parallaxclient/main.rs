3ebef7cb25280b0e07cfd25c513a364cfb2197ba
Quinten Palmer <quintenpalmer@gmail.com>
2016-12-17 17:26:36 -0500

    Introduce run_app for easier error handling
    This avoids needing to print and break/return appropriately, taking
    advantage of the try! macro and return error flows. `main` only needs to
    log success or error once and doesn't need any wonky early-return
    control flow.

--- src/parallaxclient/main.rs
+++ src/parallaxclient/main.rs
@@ -2,33 +2,29 @@ extern crate graphics;
 
 fn main() {
     println!("hello from a client");
+    match run_app() {
+        Ok(_) => println!("ran successfully"),
+        Err(err) => println!("error: {}", err),
+    };
+    println!("exiting");
+}
+
+fn run_app() -> Result<(), Box<std::error::Error>> {
     let rects = vec![graphics::Rect::new(0.0, 0.0, 0.2, 0.6),
                      graphics::Rect::new(0.0, 0.0, 0.6, 0.2)];
-    let r_app = graphics::App::new(600,
-                                   600,
-                                   "Parallax Client Demo",
-                                   graphics::SIMPLE_VERTEX_SOURCE,
-                                   graphics::SIMPLE_FRAGMENT_SOURCE,
-                                   rects);
-    match r_app {
-        Ok(app) => {
-            loop {
-                match app.draw() {
-                    Ok(_) => {}
-                    Err(err) => {
-                        println!("error: {}", err);
-                        break;
-                    }
-                };
-                if app.window.handle_events() {
-                    break;
-                }
-            }
-            app.close();
-        }
-        Err(err) => {
-            println!("could not create window: {}", err);
+
+    let app = try!(graphics::App::new(600,
+                                      600,
+                                      "Parallax Client Demo",
+                                      graphics::SIMPLE_VERTEX_SOURCE,
+                                      graphics::SIMPLE_FRAGMENT_SOURCE,
+                                      rects));
+    loop {
+        try!(app.draw());
+        if app.window.handle_events() {
+            break;
         }
     }
-    println!("exiting");
+    app.close();
+    return Ok(());
 }
