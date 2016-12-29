752d4eebd504650cee01643b52e11ebba9c0bbb3
Quinten Palmer <quintenpalmer@gmail.com>
2016-12-29 01:13:21 -0500

    Generate png for mandelbrot set
    The concept the Frame introduces is that given a screen definition (it's
    width and height) and x and y ranges, we can map a given pixel to where
    it represents in the x and y coords. Using this and the image library we
    are able to determine whether a pixel should be filled in or not based
    on its divergence from the function of the previous commit. The pieces
    likely to change here in the future are determining how colorful to make
    something based on its divergence, with the pixel construction in
    `gen_pixel` being what will change the most. We offer the ability to get
    an ImageRgba8 and do with it what you will, or write the image to a hard
    coded file, this will be parameterized soon.

--- lib/mandelbrot/src/lib.rs
+++ lib/mandelbrot/src/lib.rs
@@ -1,5 +1,10 @@
 extern crate num;
+extern crate image;
 
+mod image_create;
 mod eq;
 
+pub use image_create::gen_png;
+pub use image_create::write_png;
+pub use image_create::Frame;
 pub use eq::mandelbrot_divergence;
